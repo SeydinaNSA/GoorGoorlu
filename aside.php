ae647be99f535e0fd4535eb58ef5f507ba4981f3
DiagneMouhamed <djira04@gmail.com>
2017-03-30 08:38:30 +0200

    ajout aside.php

---
+++ aside.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<link rel="stylesheet" type="text/css" href="Style/styleAccueil.css">
+</head>
+<body>
+		<div class="service">
+                     <p>Services</p>
+						<div class="services-list-container">
+							<ul class="service-list">
+								<li><a href=".html" ><span>Electricité</span></a></li>
+								<li><a href=".html" ><span>Mecanique</span></a></li>
+								<li><a href=".html" ><span>Menuiserie</span></a></li>
+								<li><a href=".html" ><span>Plomberie</span></a></li>
+
+							</ul>
+						</div>			
+
+			
+		</div>
+</body>
+</html>
+ 
